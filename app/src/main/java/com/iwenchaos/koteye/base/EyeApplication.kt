0ae7c66b58b682446e74c0d70418259bfb807cb5
iwenchao <iwenchaos@gmail.com>
2018-04-20 10:42:47 +0800

    增加依赖文件

---
+++ app/src/main/java/com/iwenchaos/koteye/base/EyeApplication.kt
@@ -0,0 +1,17 @@
+package com.iwenchaos.koteye.base
+
+import android.app.Application
+
+/**
+ * Created by chaos
+ * on 2018/4/20. 10:30
+ * 文件描述：
+ */
+class EyeApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
