d4448ca4ef00b14b6c3d3fa06a67a98c764a2141
iwenchao <iwenchaos@gmail.com>
2018-04-20 11:03:47 +0800

    增加依赖文件;base类

---
+++ app/src/main/java/com/iwenchaos/koteye/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.iwenchaos.koteye.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by chaos
+ * on 2018/4/20. 10:56
+ * 文件描述：
+ */
+abstract class BaseActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/iwenchaos/koteye/ui/activity/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.iwenchaos.koteye.ui.activity
+
+import android.os.Bundle
+import com.iwenchaos.koteye.base.BaseActivity
+
+/**
+ * Created by chaos
+ * on 2018/4/20. 10:56
+ * 文件描述：
+ */
+class SplashActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
