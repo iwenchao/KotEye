32541a28a23057f8d31400cbda616e89078b6531
iwenchao <iwenchaos@gmail.com>
2018-04-20 13:55:40 +0800

    增加logger库的使用

--- app/src/main/java/com/iwenchaos/koteye/base/EyeApplication.kt
+++ app/src/main/java/com/iwenchaos/koteye/base/EyeApplication.kt
@@ -1,6 +1,10 @@
 package com.iwenchaos.koteye.base
 
 import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.BuildConfig
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
 
@@ -10,12 +14,14 @@ import com.squareup.leakcanary.RefWatcher
  * 文件描述：
  */
 class EyeApplication : Application() {
+    private val EYEAPPLICATION: String = "EyeApplication"
 
     private var refWatcher: RefWatcher? = null
 
     override fun onCreate() {
         super.onCreate()
         refWatcher = leackCanary()
+        logger()
     }
 
     private fun leackCanary(): RefWatcher? =
@@ -25,7 +31,19 @@ class EyeApplication : Application() {
                 LeakCanary.install(this)
 
 
-
+    private fun logger() {
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(true)
+                .methodCount(3)
+                .methodOffset(5)
+                .tag(EYEAPPLICATION)
+                .build()
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
+    }
 
 
 }
